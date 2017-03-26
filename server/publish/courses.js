6cbb647b39bd5a56237b5a491c3f069e700c5aea
Cassiano Vellames <c.vellames@outlook.com>
2017-03-26 20:17:05 -0300

    permissions finish

--- server/publish/courses.js
+++ server/publish/courses.js
@@ -1,5 +1,21 @@
 import {Courses} from "../../models/courses"
 
-Meteor.publish("courses.select", () => {
-    return Courses.find();
+/**
+ * Publish the courses
+ * If user are'nt admin, only publish the courses assigned to it
+ */
+Meteor.publish("courses.select", function() {
+    if(this.userId){
+        const user = Meteor.users.find({"_id" : this.userId}).fetch()[0];
+
+        if(!user.profile.isAdmin){
+            var courses = [];
+            for(var i = 0; i < user.profile.courses.length; i++){
+                courses.push(user.profile.courses[i]._id);
+            }
+
+            return Courses.find({"_id" : {$in : courses}});
+        }
+        return Courses.find();
+    }
 });
--- server/publish/questions.js
+++ server/publish/questions.js
@@ -1,5 +1,12 @@
 import {Questions} from "../../models/questions"
 
-Meteor.publish("questions.select", () => {
-    return Questions.find();
+/**
+ * Questions publish
+ * If user are'nt admin, only publish the questions created by it
+ */
+Meteor.publish("questions.select", function() {
+    if(this.userId){
+        const user = Meteor.users.find({"_id" : this.userId}).fetch()[0];
+        return (user.profile.isAdmin ? Questions.find() : Questions.find({"createdBy._id" : this.userId}));
+    }
 });
