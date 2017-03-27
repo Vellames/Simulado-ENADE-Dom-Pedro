00724e15ee8d54dfb0415096f65cf7c30066be9a
Cassiano Vellames <cassiano.vellames@topos.local>
2017-03-27 12:34:34 -0300

    Fix i18n accounts-ui-bootstrap

--- client/events/header.js
+++ client/events/header.js
@@ -1,6 +1,12 @@
 Template.header.events({
     "click [data-i18n]" : (event, instance) => {
-        const language = $(event.currentTarget).attr("data-i18n");
+        var language = $(event.currentTarget).attr("data-i18n");
         TAPi18n.setLanguage(language);
+
+        if(language == "br"){
+            language = "pt-BR";
+        }
+
+        accountsUIBootstrap3.setLanguage(language);
     }
 });
