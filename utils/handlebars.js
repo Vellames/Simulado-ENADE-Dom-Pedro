67b84a8edda4fd8d32b78cccdbcb3e6fb56829ca
Cassiano Vellames <c.vellames@outlook.com>
2017-03-31 18:35:03 -0300

    Cut question string in list

--- utils/handlebars.js
+++ utils/handlebars.js
@@ -5,3 +5,10 @@ Handlebars.registerHelper("ternary", function(value, yes, no){
 Handlebars.registerHelper("ternaryEquals", function(value, equals,  yes, no){
     return (value == equals) ? yes: no;
 });
+
+Handlebars.registerHelper('cutString', function(string) {
+    if(string.length > 100){
+        string = string.substring(0,50) + "...";
+    }
+    return new Handlebars.SafeString(string);
+});
